4df5403173d459d121572e9fdc8f4e7b63b4e5af
tusharthorve <tusharthorve@yahoo.com>
2021-03-15 21:59:03 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ctc1b4-t8t9jk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ctc1b4-t8t9jk)
---
+++ src/app/products.ts
@@ -0,0 +1,26 @@
+export const products = [
+  {
+    id: 1,
+    name: "Veg Plan",
+    price: 299.99,
+    description: "All Vegan ingredients"
+  },
+  {
+    id: 2,
+    name: "Non Veg Plan",
+    price: 299.99,
+    description: "Vegan plus non-vegeterian ingredients"
+  },
+  {
+    id: 3,
+    name: "Custom",
+    price: 299.99,
+    description: "You make your own plan"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
